0a9e49ed94204e97dcb197be80ae0f12e1885d98
akhorevich <a.khorevich@gmail.com>
2015-11-26 13:40:38 +0300

    update ListView

--- app/src/main/java/com/example/user/elwica/MainActivity.java
+++ app/src/main/java/com/example/user/elwica/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.user.elwica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listMain;
+    String[] mainArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainArray = getResources().getStringArray(R.array.mainArray);
+        View head = getLayoutInflater().inflate(R.layout.header, null);
+        listMain = (ListView) findViewById(R.id.listMain);
+
+        listMain.addHeaderView(head);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, mainArray);
+        listMain.setAdapter(adapter);
+
     }
 }
