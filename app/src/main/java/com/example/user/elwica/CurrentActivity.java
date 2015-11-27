b0f2f004b1015660e363c29c6e23063f292e7601
Nortti <a.khorevich@gmail.com>
2015-11-27 07:01:07 +0300

    Add new Activity - Current

---
+++ app/src/main/java/com/example/user/elwica/CurrentActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.elwica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CurrentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.current);
+
+    }
+}
