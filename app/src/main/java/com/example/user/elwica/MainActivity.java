3ff360275b6aac1be32e21a7f1d3fd4fa1d444b3
Nortti <a.khorevich@gmail.com>
2015-11-28 21:00:37 +0300

    Current Update

--- app/src/main/java/com/example/user/elwica/MainActivity.java
+++ app/src/main/java/com/example/user/elwica/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.user.elwica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements OnItemClickListener{
     ListView listMain;
     String[] mainArray;
 
@@ -21,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
         listMain.addHeaderView(head);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, mainArray);
         listMain.setAdapter(adapter);
+        listMain.setOnItemClickListener(this);
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch(position){
+            case 0:
+                Intent i = new Intent(this, CurrentActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
